60becd7520a9ab84bf7ba118f8524d5ee4a49bda
Yekutiel Katanov <yekutielkatanov@gmail.com>
2023-12-19 17:51:10 +0200

    yes

--- src/reducers/statsReducer.ts
+++ src/reducers/statsReducer.ts
@@ -12,7 +12,7 @@ export const statsReducer = (stats = initialStats, action: Action): Stats => {
     switch (action.type) {
         case CHANGE_STATS:
             const statsType = action.payload.statsType as StatsType
-            let res = stats[statsType] + (action.payload.isIncrement ? 1 : -1);
+            let res = stats.stats[statsType] + (action.payload.isIncrement ? 1 : -1);
             res = res < 0 ? 0 : res
             return {...stats, [statsType] : res}
 
--- src/reducers/userReducer.ts
+++ src/reducers/userReducer.ts
@@ -1,4 +1,4 @@
-import {Action, State, User} from "../utils/type";
+import {Action, User} from "../utils/type";
 import {CHANGE_AVATAR, CHANGE_NAME} from "../actions/userActions";
 
 export const initialUser: User = {
@@ -11,9 +11,9 @@ export const initialUser: User = {
 export const userReducer = (user = initialUser, action: Action): User => {
     switch (action.type) {
         case CHANGE_NAME:
-            return {...user, name: action.payload || user.name}
+            return {...user, user: action.payload || user.user.name}
         case CHANGE_AVATAR:
-            return {...user, avatar: action.payload || user.avatar}
+            return {...user, user: action.payload || user.user.avatar}
         default:
             return user
     }
